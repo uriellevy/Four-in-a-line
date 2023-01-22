e49068c642b566b1b2f7e9641f7b27c1138bd69a
uriellevy <uriellevy4892@gmail.com>
2023-01-22 16:11:11 +0200

    initial game functionallity added

--- script.js
+++ script.js
@@ -1,3 +1,43 @@
 "use strict";
-const a = 5;
-console.log(a);
+const playerOne = "first";
+const playerTwo = "second";
+let currPlayer = playerOne;
+let gameOver = false;
+let board;
+const rows = 6;
+const columns = 7;
+let currColumns = [];
+window.onload = function () {
+    setGame();
+};
+const setGame = () => {
+    var _a;
+    currColumns = [5, 5, 5, 5, 5, 5, 5];
+    board = [];
+    for (let r = 0; r < rows; r++) {
+        let row = [];
+        for (let c = 0; c < columns; c++) {
+            row.push(" ");
+            const tile = document.createElement("div");
+            tile.id = r.toString() + "-" + c.toString();
+            tile.classList.add("tile");
+            tile.addEventListener("click", setPiece);
+            (_a = document.getElementById("board")) === null || _a === void 0 ? void 0 : _a.append(tile);
+        }
+        board.push(row);
+    }
+};
+function setPiece(event) {
+    if (gameOver)
+        return;
+    //get coords of tile clicked
+    const elementCoords = event.srcElement.id.split("-");
+    let r = parseInt(elementCoords[0]);
+    const c = parseInt(elementCoords[1]);
+    // figure out which row the current column should be on
+    r = currColumns[c];
+    if (r < 0) { // board[r][c] != ' '
+        return;
+    }
+    console.log(board);
+}
--- script.ts
+++ script.ts
@@ -1,2 +1,56 @@
-const a:number = 5;
-console.log(a)
+type Player = "first" | "second";
+const playerOne: Player = "first";
+const playerTwo: Player = "second";
+let currPlayer: Player = playerOne;
+
+let gameOver = false;
+let board: any[];
+
+const rows = 6;
+const columns = 7;
+let currColumns: number[] = [];
+
+window.onload = function () {
+    setGame();
+}
+
+const setGame = () => {
+    currColumns = [5, 5, 5, 5, 5, 5, 5];
+    board = [];
+    for (let r = 0; r < rows; r++) {
+        let row: string[] = [];
+        for (let c = 0; c < columns; c++) {
+            row.push(" ");
+            const tile = document.createElement("div");
+            tile.id = r.toString() + "-" + c.toString();
+            tile.classList.add("tile");
+            tile.addEventListener("click", setPiece)
+            document.getElementById("board")?.append(tile);
+        }
+        board.push(row);
+    }
+}
+
+function setPiece(event: any) {
+    if (gameOver) return;
+
+    //get coords of tile clicked
+    const elementCoords = event.srcElement.id.split("-");
+    let r = parseInt(elementCoords[0]);
+    const c = parseInt(elementCoords[1]);
+
+    // figure out which row the current column should be on
+    r = currColumns[c]
+
+    if (r < 0) { // board[r][c] != ' '
+        return;
+    }
+
+    console.log(board)
+
+
+
+}
+
+
+
